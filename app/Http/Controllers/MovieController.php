f4aa1f4aea2ee04c2d5d7545af91c127f0eb6ac2
Freddy Tomada <freddytomada7@gmail.com>
2026-09-20 18:26:53 -0400

    Add store, show, update and delete methods in MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMovieRequest;
+use App\Http\Requests\UpdateMovieRequest;
 use App\Http\Resources\MovieResource;
+use App\Models\Movie;
 use App\Repositories\Contracts\MovieRepositoryInterface;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
@@ -34,9 +37,19 @@ class MovieController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreMovieRequest $request)
     {
-        //
+        $movie = $this->movieRepository->create(
+            $request->safe()->except('genre_ids')
+        );
+
+        if ($request->has('genre_ids')) {
+            $this->movieRepository->syncGenres($movie, $request->genre_ids);
+        }
+
+        $movie->load('genres');
+
+        return $this->successResponse(new MovieResource($movie), $movie->title.' created successfully');
     }
 
     /**
@@ -44,22 +57,36 @@ class MovieController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $movie = $this->movieRepository->findOrFail($id);
+
+        $movie->load('genres');
+
+        return $this->successResponse(new MovieResource($movie));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateMovieRequest $request, Movie $movie)
     {
-        //
+        $movie = $this->movieRepository->update($movie, $request->safe()->except('genre_ids'));
+
+        if ($request->has('genre_ids')) {
+            $this->movieRepository->syncGenres($movie, $request->genre_ids);
+        }
+
+        $movie->load('genres');
+
+        return $this->successResponse(new MovieResource($movie), $movie->title.' updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Movie $movie)
     {
-        //
+        $this->movieRepository->delete($movie);
+
+        return response()->noContent();
     }
 }
