baf55ebb96be9c529677a2f289ef0b6366fcc7e4
Freddy Tomada <freddytomada7@gmail.com>
2026-09-21 10:37:33 -0400

    Add MovieFactory and MovieTest for movie creation and management

---
+++ tests/Feature/MovieTest.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\Movie;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('returns all movies on index', function () {
+    Movie::factory()->count(3)->create();
+    $response = $this->getJson('api/movies');
+
+    $response->assertStatus(200);
+    $response->assertJsonCount(3, 'data');
+});
+
+it('returns a single movie on show', function () {
+    $movie = Movie::factory()->create();
+    $response = $this->getJson("api/movies/{$movie->id}");
+
+    $response->assertStatus(200);
+    $response->assertJsonPath('data.id', $movie->id);
+});
+
+it('creates a movie with auto slug on store', function () {
+    $payload = [
+        'title' => 'Chucky',
+        'year' => '1988',
+        'rating' => '8.5',
+    ];
+
+    $response = $this->postJson('api/movies', $payload);
+
+    $response->assertStatus(201);
+    $response->assertJsonPath('data.title', 'Chucky');
+    $response->assertJsonPath('data.slug', 'chucky');
+
+    $this->assertDatabaseHas('movies', [
+        'title' => 'Chucky',
+        'year' => '1988',
+        'rating' => '8.5',
+        'slug' => 'chucky',
+    ]);
+});
+
+it('modifies a movie with auto slug on update', function () {
+    $movie = Movie::factory()->create(['title' => 'Chucky', 'year' => 1988, 'rating' => 8]);
+
+    $response = $this->putJson("api/movies/{$movie->id}", ['title' => 'Child\'s Play']);
+    // dd($response->content());
+    $response->assertStatus(200)
+        ->assertJsonPath('data.title', 'Child\'s Play')
+        ->assertJsonPath('data.slug', 'childs-play')
+        ->assertJsonPath('data.year', 1988);
+});
+
+it('soft deletes a movie on destroy', function () {
+    $movie = Movie::factory()->create();
+
+    $this->deleteJson("api/movies/{$movie->id}");
+
+    $this->assertSoftDeleted('movies', ['id' => $movie->id]);
+});
