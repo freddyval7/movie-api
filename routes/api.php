10b50122bd06f2847dee0711e6652a20290493cd
Freddy Tomada <freddytomada7@gmail.com>
2026-09-22 16:07:33 -0400

    Add authentication routes for user registration, login, logout, refresh, and profile retrieval

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\HealthController;
 use App\Http\Controllers\MovieController;
@@ -14,3 +15,19 @@ Route::post('/genres/{id}/restore', [GenreController::class, 'restore']);
 Route::get('/genres/slug/{slug}', [GenreController::class, 'showBySlug']);
 
 Route::apiResource('movies', MovieController::class);
+
+// Public routes
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+// Protected routes
+Route::prefix('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::post('refresh', [AuthController::class, 'refresh']);
+
+    Route::get('me', [AuthController::class, 'me']);
+});
