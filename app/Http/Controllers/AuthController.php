cf4855acbc58b4620f9a42678c0823b94a8e90a6
Freddy Tomada <freddytomada7@gmail.com>
2026-09-22 15:45:15 -0400

    Refactor register method to handle token creation errors and improve error responses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Auth;
+use PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException;
 
 class AuthController extends Controller
 {
@@ -16,12 +17,21 @@ class AuthController extends Controller
     {
         $user = User::create($request->validated());
 
-        $token = Auth::guard('api')->login($user);
+        try {
+            $token = Auth::guard('api')->login($user);
 
-        return $this->successResponse(
-            $this->tokenPayload($user, $token), 'User registered successfully',
-            201
-        );
+            if (! $token) {
+                throw new JWTException('Could not create a new token');
+            }
+
+            return $this->successResponse(
+                $this->tokenPayload($user, $token), 'User registered successfully',
+                201
+            );
+        } catch (JWTException $th) {
+            return $this->errorResponse($th->getMessage(), $th->getCode());
+        }
+
     }
 
     public function tokenPayload(User $user, string $token): array
