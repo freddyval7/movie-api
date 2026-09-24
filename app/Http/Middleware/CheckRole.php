0c11b290a0437cd5bb78e23798a7f4fda38bf509
Freddy Tomada <freddytomada7@gmail.com>
2026-09-24 16:04:46 -0400

    Add CheckRole middleware for role-based access control

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $user = $request->user();
+
+        if (! $user || ! in_array($user->role, $roles)) {
+            return response()->json(['message' => "You don't have permission to do this"], 403);
+        }
+
+        return $next($request);
+    }
+}
