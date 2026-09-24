d066683159de7974c19c32143b4f593e9ef35a33
Freddy Tomada <freddytomada7@gmail.com>
2026-09-24 16:19:03 -0400

    Refactor API routes for genres and movies, replacing apiResource with explicit routes and adding store, update, and destroy methods for movies

--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health', HealthController::class);
 
-Route::apiResource('genres', GenreController::class);
-
-Route::post('/genres/{id}/restore', [GenreController::class, 'restore']);
-
-Route::get('/genres/slug/{slug}', [GenreController::class, 'showBySlug']);
-
-Route::apiResource('movies', MovieController::class);
-
 // Public routes
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
@@ -23,11 +15,46 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
+// Genres
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/slug/{slug}', [GenreController::class, 'showBySlug']);
+Route::get('/genres/{genre}', [GenreController::class, 'show']);
+
+// Movies
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{movie}', [MovieController::class, 'show']);
+
 // Protected routes
-Route::prefix('auth:api')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
 
-    Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
 
-    Route::get('me', [AuthController::class, 'me']);
+        Route::get('me', [AuthController::class, 'me']);
+    });
+
+    // Genres
+    Route::middleware('role:admin,editor')->group(function () {
+        Route::post('genres', [GenreController::class, 'store']);
+        Route::put('genres/{genre}', [GenreController::class, 'update']);
+        Route::patch('genres/{genre}', [GenreController::class, 'update']);
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        Route::delete('genres/{genre}', [GenreController::class, 'destroy']);
+        Route::post('/genres/{id}/restore', [GenreController::class, 'restore']);
+    });
+
+    // Movies
+    Route::middleware('role:admin,editor')->group(function () {
+        Route::post('movies', [MovieController::class, 'store']);
+        Route::put('movies/{movie}', [MovieController::class, 'update']);
+        Route::patch('movies/{movie}', [MovieController::class, 'update']);
+    });
+
+    Route::middleware('role:admin')->group(function () {
+        Route::delete('movies/{movie}', [MovieController::class, 'destroy']);
+        Route::post('/movies/{id}/restore', [MovieController::class, 'restore']);
+    });
 });
